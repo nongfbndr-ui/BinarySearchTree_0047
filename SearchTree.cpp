6e00692e103a8d505eecbf9fc5261e2f301d78c9
Moreno <nongfbndr@gmail.com>
2026-06-11 15:57:47 +0700

    menambahkan if dan else if

--- SearchTree.cpp
+++ SearchTree.cpp
@@ -48,5 +48,14 @@ public:
             return;
         }
 
-        
+        if (element < parent->info)
+        {
+            parent->leftchild = newNode;
+        }
+        else if (element > parent->info)
+        {
+            parent->rightchild = newNode;
+        }
+    }
+
     
