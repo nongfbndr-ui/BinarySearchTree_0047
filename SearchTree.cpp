05fa54e309803f653d4037d9f417e164653d29d6
Moreno <nongfbndr@gmail.com>
2026-06-11 16:06:26 +0700

    menambahkan case 1

--- SearchTree.cpp
+++ SearchTree.cpp
@@ -148,4 +148,13 @@ int main()
         
         switch (ch)
         {
+        case '1':
+        {
+            cout << "Enter a word: ";
+            string word;
+            cin >> word;
+            obj.insert(word);
+            break;
+        }
+
         
