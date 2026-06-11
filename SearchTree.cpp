f1b7b03f69ac1020f2fd2499114d9621d887cfc8
Moreno <nongfbndr@gmail.com>
2026-06-11 16:00:47 +0700

    menambahkan void inorder

--- SearchTree.cpp
+++ SearchTree.cpp
@@ -76,4 +76,20 @@ public:
         }
     }
 
+    void inorder(Node *ptr)
+    {
+        if (root == NULL)
+        {
+            cout << "Tree is empty" << endl;
+            return;
+        }
+
+        if (ptr != NULL)
+        {
+            inorder(ptr->leftchild);
+            cout << ptr->info << " ";
+            inorder(ptr->rightchild);
+        }
+    }
+
     
